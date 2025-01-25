6d6a4b195728d95b0cb4b0f0243a433991a5b0d0
Zekai <leonhuangzekai@hotmail.com>
2025-01-25 15:27:40 +0800

    update gold medals statistics

--- data_wash.py
+++ data_wash.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the dataset
-file_path = '/Users/zekaihuang/Desktop/MCM/MCM2025/Documents/Y2sem2/2025_Problem_C_Data/gold_medals_by_sport_and_country.csv'  # Replace with the actual path to your file
+file_path = '/Users/zekaihuang/Desktop/MCM2025/updated_gold_medals.csv'  # Replace with the actual path to your file
 data = pd.read_csv(file_path)
 
 # Identify numeric columns
@@ -11,7 +11,7 @@ numeric_columns = data.select_dtypes(include=['float', 'int']).columns
 data[numeric_columns] = data[numeric_columns].astype(int)
 
 # Save the modified DataFrame
-output_path = '/Users/zekaihuang/Desktop/MCM/MCM2025/Documents/Y2sem2/2025_Problem_C_Data/gold_medals_by_sport_and_country_updated.csv'  # Replace with the desired output path
+output_path = '/Users/zekaihuang/Desktop/MCM2025/updated_gold_medals_int.csv'  # Replace with the desired output path
 data.to_csv(output_path, index=False)
 
 print(f"Modified file saved at: {output_path}")
